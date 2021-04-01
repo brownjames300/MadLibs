6de37b62b43316627da74d0fabda5132c81797a0
James Brown <brownjames6571@gmail.com>
2021-04-01 10:30:57 +0100

    Finished!

---
+++ main.py
@@ -0,0 +1,10 @@
+# MadLibs!
+
+fName = input('Enter your first name: ')
+lName = input('Enter your last name: ')
+color = input('What is your favourite color?: ')
+speed = input('Type a random number: ')
+adjective = input('What would you describe yourself as?: ')
+
+lib = f'Hi my name is {fName} {lName} and I drive a {color} car! I like to go {speed} mph because I am {adjective}'
+print(lib)
